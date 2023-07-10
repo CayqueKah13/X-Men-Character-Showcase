193996542a30ac2d417d235cc18da2a399c60857
Cayque Martins <123041280+CayqueKah13@users.noreply.github.com>
2023-07-10 20:54:25 -0300

    smaller images and index.js update
    Something is causing the images to crash in Github, so I changed to .webp images

--- src/js/index.js
+++ src/js/index.js
@@ -61,7 +61,7 @@ function alterarImagemPersonagemSelecionado(personagem) {
     const imagemPersonagemGrande = document.querySelector('.personagem-grande');
     // passo 2 - alterar a imagem do personagem grande
     const idPersonagem = personagem.attributes.id.value;
-    imagemPersonagemGrande.src = `./src/imagens/card-${idPersonagem}.png`;
+    imagemPersonagemGrande.src = `./src/imagens/card-${idPersonagem}.webp`;
 }
 
 function removerSelecaoDoPersonagem() {
